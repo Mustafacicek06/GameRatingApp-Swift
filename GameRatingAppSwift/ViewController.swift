0f353d89af3d0731616391499b838e133cf06eef
Mustafa Çiçek <f191220093@ktun.edu.tr>
2022-09-08 00:18:26 +0300

    Initial Commit

---
+++ GameRatingAppSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameRatingAppSwift
+//
+//  Created by Mustafa Çiçek on 8.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
