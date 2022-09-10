dcc53b45aee338d9f0a55f07f9faaa0ad0eeb5e9
Mustafa Çiçek <f191220093@ktun.edu.tr>
2022-09-10 19:27:59 +0300

    uıpage view controller added.

--- GameRatingAppSwift/Core/Extension/UIScrollView+.swift
+++ GameRatingAppSwift/Core/Extension/UIScrollView+.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 
-public extension UIView {
-    func pinTo(_ view: UIView) {
+public extension UIScrollView {
+    func edgeTo(view: UIView) {
         translatesAutoresizingMaskIntoConstraints = false
-        heightAnchor.constraint(equalToConstant: 50).isActive = true
+        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
+
+extension UIScrollView {
+    func scrollTo(horizontalPage: Int? = 0, verticalPage: Int? = 0, animated: Bool? = true) {
+        var frame: CGRect = self.frame
+        frame.origin.x = frame.size.width * CGFloat(horizontalPage ?? 0)
+        frame.origin.y = frame.size.width * CGFloat(verticalPage ?? 0)
+        self.scrollRectToVisible(frame, animated: animated ?? true)
     }
 }
--- GameRatingAppSwift/Features/Home/View/HomeVC.swift
+++ GameRatingAppSwift/Features/Home/View/HomeVC.swift
@@ -6,72 +6,92 @@
 //
 
 import UIKit
+import Kingfisher
 
 class HomeVC: UIViewController {
+    
+       lazy var view0: UIView = {
+           let view = UIView()
+           view.backgroundColor = .systemTeal
+           let label = UILabel()
+           label.text = "Page 0"
+           label.textAlignment = .center
+           view.addSubview(label)
+           label.edgeTo(view: view)
+           return view
+       }()
+       
+       lazy var view1: UIView = {
+           let view = UIView()
+           view.backgroundColor = .systemPink
+           let label = UILabel()
+           label.text = "Page 1"
+           label.textAlignment = .center
+           view.addSubview(label)
+           label.edgeTo(view: view)
+           return view
+       }()
+       
+       lazy var view2: UIView = {
+           let view = UIView()
+           view.backgroundColor = .systemYellow
+           let label = UILabel()
+           label.text = "Page 2"
+           label.textAlignment = .center
+           view.addSubview(label)
+           label.edgeTo(view: view)
+           return view
+       }()
+       
+       lazy var views = [view0, view1, view2]
+       
+       lazy var scrollView: UIScrollView = {
+           let scrollView = UIScrollView()
+           scrollView.showsHorizontalScrollIndicator = false
+           scrollView.isPagingEnabled = true
+           scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(views.count), height: view.frame.height/3)
+           
+           for i in 0..<views.count {
+               scrollView.addSubview(views[i])
+               views[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height/3)
+           }
+           
+           scrollView.delegate = self
+           
+           return scrollView
+       }()
+       
+       lazy var pageControl: UIPageControl = {
+           let pageControl = UIPageControl()
+           pageControl.numberOfPages = views.count
+           pageControl.currentPage = 0
+           pageControl.addTarget(self, action: #selector(pageControlTapHandler(sender:)), for: .touchUpInside)
+           return pageControl
+       }()
+       
+       @objc
+       func pageControlTapHandler(sender: UIPageControl) {
+           scrollView.scrollTo(horizontalPage: sender.currentPage, animated: true)
+       }
 
-    
-    private let messageLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Downloading.."
-        
-        label.font = UIFont.systemFont(ofSize: 20)
-        label.textColor = .black
-        
-        label.textAlignment = .center
-        
-        return label
-        
-    }()
-    
-    private let textField: UITextField = {
-       let field = UITextField()
-        field.text = "Search"
-        field.borderStyle = .bezel
-        field.
-        field.textColor = .green
-        
-        return field
-    }()
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor  = .white
-        // Do any additional setup after loading the view.
-        view.addSubview(messageLabel)
-        view.addSubview(textField)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target:self, action:  #selector(searchButtonClicked))
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        // responsive'ligi nasıl yapılıyor ona bak
-        messageLabel.frame = CGRect(x: view.frame.width/2 - 100 , y: view.frame.height/2 - 25, width: 200, height: 50)
-        textField.frame = CGRect(x: view.frame.width/2  , y: view.frame.height/2 , width: 200, height: 50)
-        
-        /*
-         let layout = view.layoutMarginsGuide
-         
-         searchButton.translatesAutoresizingMaskIntoConstraints = False
-         searchButton.topAnchor.constraint(equalTo: layout.topAnchor).isActive = true
-         searchButton.leadingAnchor.constraint(equalTo: layout.leadingAnchor).isActive = true
-         
-         */
-    }
-    @objc func searchButtonClicked() {
-        
-    }
-    
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           // Do any additional setup after loading the view.
+           view.backgroundColor = .white
+           
+           view.addSubview(scrollView)
+           scrollView.edgeTo(view: view)
+           
+           view.addSubview(pageControl)
+           pageControl.pinTo(view)
+       }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   }
 
-}
+   extension HomeVC: UIScrollViewDelegate {
+       func scrollViewDidScroll(_ scrollView: UIScrollView) {
+           let pageIndex = round(scrollView.contentOffset.x / view.frame.width)
+           pageControl.currentPage = Int(pageIndex)
+       }
+   }
