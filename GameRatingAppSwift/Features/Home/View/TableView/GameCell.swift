98326d123ecb068569e64f3745915c01d9b609db
Mustafa Çiçek <f191220093@ktun.edu.tr>
2022-10-07 02:19:17 +0300

    rating added.

--- GameRatingAppSwift/Features/Home/View/TableView/GameCell.swift
+++ GameRatingAppSwift/Features/Home/View/TableView/GameCell.swift
@@ -38,7 +38,10 @@ class GameCell: UITableViewCell {
             gameImageView.sd_setImage(with: URL(string: game.backgroundImage!))
             gameTitleLabel.text = game.name
             if let releaseDate = game.released {
-                gameDescriptionLabel.text = releaseDate
+                if let rating = game.rating {
+                    gameDescriptionLabel.text = "\(rating)  \(releaseDate)"
+                }
+           
             
             }
          
@@ -78,7 +81,7 @@ class GameCell: UITableViewCell {
     func setTitleLabelConstraints() {
         gameTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            gameTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant:  -7),
+            gameTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant:  -10),
             gameTitleLabel.leadingAnchor.constraint(equalTo: gameImageView.trailingAnchor,constant: 20),
             gameTitleLabel.heightAnchor.constraint(equalToConstant: 50),
             gameTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -12)
@@ -89,7 +92,7 @@ class GameCell: UITableViewCell {
     func setDescriptionLabelConstraints() {
         gameDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            gameDescriptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor,constant: 10),
+            gameDescriptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor,constant: 15),
             gameDescriptionLabel.leadingAnchor.constraint(equalTo: gameImageView.trailingAnchor,constant: 20),
             gameDescriptionLabel.heightAnchor.constraint(equalToConstant: 40),
             gameDescriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -12)
